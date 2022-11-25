9e2ded3e8d131a67ee5034c0d8625db74a284e54
JefferyBurgett <jeffery.burgett@gmail.com>
2022-11-24 21:25:56 -0600

    Created Comment App/Model, added to Settings.py

--- backend/drf_jwt_backend/settings.py
+++ backend/drf_jwt_backend/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'authentication.apps.AuthenticationConfig',
     'corsheaders',
-    'cars.apps.CarsConfig'  # This app is for example use only
+    'cars.apps.CarsConfig',
+    'comment.apps.CommentConfig',  # This app is for example use only
 ]
 
 MIDDLEWARE = [
